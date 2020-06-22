3764033a8f9a218f1279af763b12d156358f3a38
Eduardo <eduardo.requena@pucp.pe>
2020-06-22 13:09:14 -0500

    BACKUP ANTES DEL LAB

--- app/src/main/java/pucp/telecom/moviles/lab3/Main2Activity2.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/Main2Activity2.java
@@ -1,8 +1,13 @@
 package pucp.telecom.moviles.lab3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -17,66 +22,78 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class Main2Activity2 extends AppCompatActivity implements SensorEventListener {
 
-    SensorManager sensorManager;
-    Button btnRecord;
-    Button btnStop;
-    MediaRecorder mediaRecorder;
-    //MediaPlayer mediaPlayer;
+    private SensorManager sensorManager;
+    private Button btnRecord;
+    private Button btnStop;
+    private Button btnPlay;
+    private double mEMA = 0.0;
+    private MediaRecorder mediaRecorder ;
+    private int codigoPermisoWriteReadSD = 1;
+    //private MediaPlayer mediaPlayer;
+    String outputFile;
 
-    final int REQUEST_PERMISSION_CODE = 1000;
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+    private final int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main22);
 
-        btnRecord = (Button) findViewById(R.id.buttonPlay);
-        btnStop = (Button) findViewById(R.id.buttonStop);
+        //------------------------------------------------------------------------------------------------------
+        //PERMISOS
+        if (ContextCompat.checkSelfPermission(Main2Activity2.this,
+                Manifest.permission.RECORD_AUDIO) + ContextCompat.checkSelfPermission(Main2Activity2.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
 
-        String outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Main2Activity2.this,
+                    Manifest.permission.RECORD_AUDIO) || ActivityCompat.shouldShowRequestPermissionRationale(Main2Activity2.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(Main2Activity2.this,
+                        new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE);
 
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        mediaRecorder.setOutputFile(outputFile);
 
 
-
-        btnRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    mediaRecorder.prepare();
-                    mediaRecorder.start();
-                } catch (IllegalStateException ise) {
-                    // make something ...
-                } catch (IOException ioe) {
-                    // make something
-                }
-
-                //record.setEnabled(false);
-                //stop.setEnabled(true);
-                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
             }
-        });
+        } else {
+
+
+            // Permission has already been granted
+            btnRecord = (Button) findViewById(R.id.buttonRecord);
+            btnStop = (Button) findViewById(R.id.buttonStop);
+           // btnPlay = (Button) findViewById(R.id.buttonPlay);
+           // btnStop.setEnabled(false);
+            //btnPlay.setEnabled(false);
 
 
 
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaRecorder.stop();
-                mediaRecorder.release();
-                mediaRecorder = null;
-             //   record.setEnabled(true);
-              //  stop.setEnabled(false);
-               // play.setEnabled(true);
-                Toast.makeText(getApplicationContext(), "Audio Recorder stopped", Toast.LENGTH_LONG).show();
-            }
-        });
+
+
+
+
+
+        }
+
+
+        //------------------------------------------------------------------------------------------------------
+
 
 
 
@@ -97,13 +114,55 @@ public class Main2Activity2 extends AppCompatActivity implements SensorEventList
 //---------------------------------------------------------------------------------------------------------
     }
 
+    public void Start(View view){
+        Long date=new Date().getTime();
+        Date current_time = new Date(Long.valueOf(date));
+        mediaRecorder = new MediaRecorder();
+        outputFile = getFilesDir() + "/"+current_time+".3gp";
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(outputFile);
+        try {
+            //mediaRecorder=new MediaRecorder();
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IllegalStateException ise) {
+            // make something ...
+        } catch (IOException ioe) {
+            // make something
+        }
+
+        btnRecord.setEnabled(false);
+        btnStop.setEnabled(true);
+        Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+        mEMA = 0.0;
+
+    }
+
+    public void Stop(View view){
+
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+        btnRecord.setEnabled(true);
+        btnStop.setEnabled(false);
+        // play.setEnabled(true);
+        Toast.makeText(getApplicationContext(), "Audio Recorder stopped", Toast.LENGTH_LONG).show();
+
+    }
+
+
+//---------------------------------------------------------------------------------------------------------
+
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         int type = event.sensor.getType();
 
         if (type == Sensor.TYPE_ACCELEROMETER) {
-            Log.d("infoAppX:", String.valueOf(event.values[0]));
-            Log.d("infoAppY:", String.valueOf(event.values[1]));
+         //   Log.d("infoAppX:", String.valueOf(event.values[0]));
+          //  Log.d("infoAppY:", String.valueOf(event.values[1]));
 
         }
     }
